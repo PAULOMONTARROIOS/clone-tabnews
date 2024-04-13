a86211b5669309dd4326f3312593a01a8f461cfc
Paulo Montarroios <pauloricardomontarroios@hotmail.com>
2024-04-13 18:57:16 +0000

    Makes database.js more robust to errror

--- infra/database.js
+++ infra/database.js
@@ -10,11 +10,19 @@ async function query(queryObject) {
   });
 
   try {
+    console.log("Credenciais do Postgres:", {
+      host: process.env.POSTGRES_HOST,
+      port: process.env.POSTGRES_PORT,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+    });
     await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (err) {
     console.error(err);
+    throw error;
   } finally {
     await client.end();
   }
